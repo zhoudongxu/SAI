0886a19863c10286411483e63f7d10ad453c10a0
richardyu-ms <richard.yu@microsoft.com>
2022-05-10 11:23:06 +0000

    init the t0 structure which can support more dimision
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

---
+++ ptf/sai_t0_case.py
@@ -0,0 +1,13 @@
+from sai_thrift.sai_headers import *
+from sai_base_test import *
+
+class InitBasicData(PlatformSaiHelper):
+    def setUp(self):
+        #this process contains the switch_init process
+        SaiHelperBase.setUp(self)
+
+    def runTest(self):
+        pass
+
+    def tearDown(self):
+        pass
